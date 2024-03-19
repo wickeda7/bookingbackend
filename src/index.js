748fd51feb07323b6fe04121007f2c6f067f8a8a
Hung <hungqto@yahoo.com>
2024-03-18 22:37:00 -0700

    added socketio

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-'use strict';
-
+"use strict";
+const HOST_URL = process.env.HOST_URL;
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +16,36 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    process.nextTick(() => {
+      strapi.log.info("Hello world!");
+      strapi.log.info(HOST_URL);
+      strapi.log.info("sdfsfsdf");
+      // @ts-ignore
+      strapi.ioServer = require("socket.io")(strapi.server.httpServer, {
+        cors: {
+          // cors setup
+          // "Allow-Origin": HOST_URL,
+          origin: [HOST_URL, "http://localhost:3000"],
+          methods: ["GET", "POST"],
+          allowedHeaders: ["my-custom-header"],
+          credentials: true,
+        },
+      });
+      strapi.ioServer.use((socket, next) => {
+        const userid = socket.handshake.auth.userid;
+        if (!userid) {
+          return next(new Error("invalid merchantId"));
+        }
+        socket.userid = userid;
+        next();
+      });
+      strapi.ioServer.on("connection", (socket) => {
+        console.log("a user connected", socket.userid);
+        socket.on("disconnect", () => {
+          console.log("user disconnected", socket.userid);
+        });
+      });
+    });
+  },
 };
