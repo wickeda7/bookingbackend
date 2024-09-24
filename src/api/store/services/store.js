f9ef686ab513ca7a56f396082310dd3f9ff489fd
Hung <hungqto@yahoo.com>
2024-09-24 15:31:26 -0700

    test

--- src/api/store/services/store.js
+++ src/api/store/services/store.js
@@ -164,6 +164,7 @@ module.exports = createCoreService("api::store.store", ({ strapi }) => ({
           },
         },
       });
+      console.log("entry", entry);
       return entry;
     } catch (error) {
       console.log(error);
